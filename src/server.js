1b190475dee705dbf96e7cc35954ae80e106c17f
Chris Pooley <cpooley@gmail.com>
2021-01-12 21:15:09 -0800

    Added body parser

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { routes } from './routes/index.js';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 routes.forEach((route) => {
   app[route.method](route.path, route.handler);
